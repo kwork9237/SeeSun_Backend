cbb1c48d4911b8805c67469263484fa7a954b5cd
kwork9237 <121546059+kwork9237@users.noreply.github.com>
2026-01-20 11:54:25 +0900

    회원가입 추가

--- src/main/java/com/seesun/dto/member/request/MemberJoinDTO.java
+++ src/main/java/com/seesun/dto/member/request/MemberJoinDTO.java
@@ -2,6 +2,8 @@ package com.seesun.dto.member.request;
 
 import org.apache.ibatis.type.Alias;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +12,7 @@ import lombok.Setter;
 @Setter
 @Alias("memberJoin")
 public class MemberJoinDTO {
+	@JsonProperty("email")		// Front에서는 Email과 매핑됨
 	private String username;	// ID
 	private String password;	// PW
 	private String name;		// Name
